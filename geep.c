e0c63956e39053106bec07c8d28ad8d814c7e5c8
Philip Jones <philj56@gmail.com>
2018-08-25 18:41:13 +0100

    Much better chords, full sailor moon

--- geep.c
+++ geep.c
@@ -14,15 +14,20 @@
 
 #define CLOCK_TICK_RATE 1193180lu
 
+#define BPM 140
+#define CROTCHET (1000000000lu * 60lu / BPM)
+#define QUAVER (1000000000lu * 60lu / BPM / 2)
+#define SEMIQUAVER (1000000000lu * 60lu / BPM / 4)
+#define DEMISEMIQUAVER (1000000000lu * 60lu / BPM / 8)
+#define SIXTH (1000000000lu * 60lu / BPM / 6)
+
 static uint64_t min(uint64_t x, uint64_t y)
 {
 	return x < y ? x : y;
 }
 
-static const int quaver = 214;
-
-void beep(int freq, int length);
-void beep_chord(int freq1, int freq2, int length);
+void beep(int freq, uint64_t length);
+void beep_chord(int freq1, int freq2, uint64_t length);
 uint64_t time_diff(const struct timespec *cur,
 		const struct timespec *old);
 void do_beep(int freq, int console_fd);
@@ -45,70 +50,458 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 	signal(SIGINT, quit);
-	beep_chord(1, g2, quaver);
-	beep_chord(1, c3, quaver);
-	beep_chord(1, eb3, quaver);
-	beep_chord(c2, g3, quaver);
-	beep_chord(g2, g3, quaver);
-	beep_chord(eb2, g3, quaver);
-	beep_chord(g2, f3, quaver);
-	beep_chord(c2, f3, quaver);
-	beep_chord(g2, f3, quaver);
-	beep_chord(eb2, eb3, quaver);
-	beep_chord(1, g2, quaver);
-	beep_chord(g1, d3, quaver);
-	beep_chord(f2, d3, quaver);
-	beep_chord(d2, f3, quaver);
-	beep_chord(f2, f3, quaver);
-	beep_chord(1, g1, quaver);
-	beep_chord(g1, b2, quaver);
-	beep_chord(d2, d3, quaver);
-	beep_chord(f2, f3, quaver);
-	beep_chord(g1, ab3, quaver);
-	beep_chord(f2, ab3, quaver);
-	beep_chord(d2, ab3, quaver);
-	beep_chord(f2, bb3, quaver);
-	beep_chord(g1, bb3, quaver);
-	beep_chord(f2, ab3, quaver);
-	beep_chord(d2, g3, quaver);
-	beep_chord(f2, f3, quaver);
-	beep_chord(c2, eb2, quaver);
-	beep_chord(g2, eb2, quaver);
-	beep_chord(eb2, g3, quaver);
-	beep_chord(g2, g3, quaver);
-	beep_chord(1, c3, quaver);
-	beep_chord(g2, g2, quaver);
-	beep_chord(eb2, c3, quaver);
-	beep_chord(g2, eb3, quaver);
-	beep_chord(c2, c4, quaver);
-	beep_chord(g2, c4, quaver);
-	beep_chord(1, eb2, quaver);
-	beep_chord(g2, c4, quaver);
-	beep_chord(c2, b3, quaver);
-	beep_chord(g2, b3, quaver);
-	beep_chord(1, eb2, quaver);
-	beep_chord(g2, b3, quaver);
-	beep_chord(f2, bb3, quaver);
-	beep_chord(c3, bb3, quaver);
-	beep_chord(ab2, ab3, quaver);
-	beep_chord(1, ab3, quaver);
-	beep(r, quaver);
-	beep_chord(1, ab3, quaver);
-	beep_chord(1, g3, quaver);
-	beep_chord(1, f3, quaver);
-	beep_chord(g1, g3, quaver);
-	beep_chord(g2, g3,  quaver);
-	beep_chord(ab1, f3, quaver);
-	beep_chord(ab2, f3, quaver);
-	beep_chord(a2, eb3, quaver);
-	beep_chord(b2, d3, quaver);
-	beep_chord(b3, d3, quaver);
-	beep_chord(c2, c3, quaver*3);
+
+	// 1
+	beep_chord(1, g4, QUAVER);
+	beep_chord(1, c5, QUAVER);
+	beep_chord(1, eb5, QUAVER);
+
+	// 2
+	beep_chord(c3, g5, QUAVER);
+	beep_chord(g3, g5, QUAVER);
+	beep_chord(eb3, g5, QUAVER);
+	beep_chord(g3, f5, QUAVER);
+	beep_chord(c3, f5, QUAVER);
+	beep_chord(g3, f5, QUAVER);
+	beep_chord(eb3, eb5, QUAVER);
+	beep_chord(1, g3, QUAVER);
+
+	// 3
+	beep_chord(g2, d5, QUAVER);
+	beep_chord(f3, d5, QUAVER);
+	beep_chord(d3, f5, QUAVER);
+	beep_chord(f3, f5, QUAVER);
+	beep_chord(g2, 1, QUAVER);
+	beep_chord(g3, b4, QUAVER);
+	beep_chord(d3, d5, QUAVER);
+	beep_chord(f3, f5, QUAVER);
+
+	// 4
+	beep_chord(g2, ab5, QUAVER);
+	beep_chord(f3, ab5, QUAVER);
+	beep_chord(d3, ab5, QUAVER);
+	beep_chord(f3, bb5, QUAVER);
+	beep_chord(g2, bb5, QUAVER);
+	beep_chord(f3, ab5, QUAVER);
+	beep_chord(d3, g5, QUAVER);
+	beep_chord(f3, f5, QUAVER);
+
+	// 5
+	beep_chord(c3, eb5, QUAVER);
+	beep_chord(g3, eb5, QUAVER);
+	beep_chord(eb3, g5, QUAVER);
+	beep_chord(g3, g5, QUAVER);
+	beep_chord(c3, 1, QUAVER);
+	beep_chord(g3, g4, QUAVER);
+	beep_chord(eb3, c5, QUAVER);
+	beep_chord(g3, eb5, QUAVER);
+
+	// 6
+	beep_chord(c3, c6, QUAVER);
+	beep_chord(g3, c6, QUAVER);
+	beep_chord(eb3, 1, QUAVER);
+	beep_chord(g3, c6, QUAVER);
+	beep_chord(c3, b5, QUAVER);
+	beep_chord(g3, b5, QUAVER);
+	beep_chord(eb3, 1, QUAVER);
+	beep_chord(g3, b5, QUAVER);
+
+	// 7
+	beep_chord(f2, bb5, QUAVER);
+	beep_chord(c3, bb5, QUAVER);
+	beep_chord(ab2, ab5, QUAVER);
+	beep_chord(1, ab5, QUAVER);
+	beep(r, QUAVER);
+	beep_chord(1, ab5, QUAVER);
+	beep_chord(1, g5, QUAVER);
+	beep_chord(1, f5, QUAVER);
+
+	// 8
+	beep_chord(g2, g5, QUAVER);
+	beep_chord(g3, g5,  QUAVER);
+	beep_chord(ab2, f5, QUAVER);
+	beep_chord(ab3, f5, QUAVER);
+	beep_chord(a2, eb5, QUAVER);
+	beep_chord(b2, d5, QUAVER);
+	beep_chord(b3, d5, QUAVER);
+	beep_chord(c3, c5, QUAVER*3);
+
+	// 9
+	beep(r, QUAVER);
+	beep_chord(g2, 1, QUAVER*3);
+	beep_chord(g2, g3, QUAVER);
+	beep_chord(g2, c4, QUAVER);
+	beep_chord(g2, eb3, QUAVER);
+
+	// 10
+	beep_chord(c3, g4, QUAVER);
+	beep_chord(g3, g4, QUAVER);
+	beep_chord(eb3, g4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(c3, f4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(eb3, eb4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+	
+	// 11
+	beep_chord(g2, d4, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+	beep_chord(d3, f4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(g2, 1, QUAVER);
+	beep_chord(f3, g3, QUAVER);
+	beep_chord(d3, b3, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+
+	// 12
+	beep_chord(g2, f4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(d3, f4, QUAVER);
+	beep_chord(f3, eb4, QUAVER);
+	beep_chord(g2, eb4, QUAVER);
+	beep_chord(f3, eb4, QUAVER);
+	beep_chord(d3, d4, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+
+	// 13
+	beep_chord(c3, c4, QUAVER);
+	beep_chord(g3, c4, QUAVER);
+	beep_chord(eb3, eb4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+	beep_chord(c3, 1, QUAVER);
+	beep_chord(g3, 1, QUAVER);
+	beep_chord(eb3, c4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+	
+	// 14
+	beep_chord(c3, g4, QUAVER);
+	beep_chord(g3, g4, QUAVER);
+	beep_chord(eb3, g4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(c3, f4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(eb3, eb4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+	
+	// 15
+	beep_chord(g2, d4, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+	beep_chord(d3, f4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(g2, 1, QUAVER);
+	beep_chord(f3, g3, QUAVER);
+	beep_chord(d3, b3, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+
+	// 16
+	beep_chord(g2, f4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(d3, g4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(g2, eb4, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+	beep_chord(d3, d4, QUAVER);
+	beep_chord(f3, eb4, QUAVER);
+
+	// 17
+	beep_chord(c3, eb4, QUAVER);
+	beep_chord(gb2, eb4, QUAVER);
+	beep_chord(g2, 1, QUAVER);
+	beep_chord(b2, 1, QUAVER);
+	beep_chord(c3, 1, QUAVER);
+	beep_chord(c3, g3, QUAVER);
+	beep_chord(c3, c4, QUAVER);
+	beep_chord(c3, eb4, QUAVER);
+
+	// 18
+	// TODO: chords
+	beep_chord(c3, g4, QUAVER);
+	beep_chord(g3, g4, QUAVER);
+	beep_chord(eb3, g4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(c3, f4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(eb3, eb4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+	
+	// 19
+	beep_chord(g2, d4, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+	beep_chord(d3, f4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(g2, 1, QUAVER);
+	beep_chord(f3, g3, QUAVER);
+	beep_chord(d3, b3, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+
+	// 20
+	beep_chord(g2, f4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(d3, g4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(g2, eb4, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+	beep_chord(d3, d4, QUAVER);
+	beep_chord(f3, eb4, QUAVER);
+
+	// 21
+	beep_chord(c3, c4, QUAVER);
+	beep_chord(g3, c4, QUAVER);
+	beep_chord(eb3, eb4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+	beep_chord(c3, 1, QUAVER);
+	beep_chord(g3, 1, QUAVER);
+	beep_chord(eb3, c4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+
+	// 22
+	beep_chord(c3, g4, QUAVER);
+	beep_chord(g3, g4, QUAVER);
+	beep_chord(eb3, g4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(c3, f4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(eb3, g4, QUAVER);
+	beep_chord(g3, g4, QUAVER);
+
+	// 23
+	beep_chord(f2, bb4, QUAVER);
+	beep_chord(c3, bb4, QUAVER);
+	beep_chord(ab2, ab4, QUAVER);
+	beep_chord(c3, ab4, QUAVER);
+	beep_chord(f2, 1, QUAVER);
+	beep_chord(c3, ab4, QUAVER);
+	beep_chord(ab2, g4, QUAVER);
+	beep_chord(c3, f4, QUAVER);
+
+	// 24
+	beep_chord(g2, g4, QUAVER);
+	beep_chord(g3, g4,  QUAVER);
+	beep_chord(ab2, f4, QUAVER);
+	beep_chord(ab3, f4, QUAVER);
+	beep_chord(a2, eb4, QUAVER);
+	beep_chord(b2, d4, QUAVER);
+	beep_chord(b3, d4, QUAVER);
+	beep_chord(c3, c4, QUAVER*3);
+
+	// 25
+	beep_chord(c3, 1, QUAVER);
+	beep_chord(c3, 1, QUAVER);
+	beep_chord(c3, 1, QUAVER);
+	beep_chord(c3, 1, QUAVER);
+	beep_chord(c3, c4, QUAVER);
+	beep_chord(c3, c4, QUAVER);
+
+	// 26
+	beep_chord(f3, ab5, QUAVER);
+	beep_chord(c4, ab5,  QUAVER);
+	beep_chord(ab3, ab5, QUAVER);
+	beep_chord(c4, ab5, QUAVER);
+	beep_chord(f3, ab5, QUAVER);
+	beep_chord(c4, ab5,  QUAVER);
+	beep_chord(ab3, bb5, QUAVER);
+	beep_chord(c4, c6, QUAVER);
+
+	// 27
+	beep_chord(f3, c6, QUAVER);
+	beep_chord(c4, c6,  QUAVER);
+	beep_chord(ab3, c6, QUAVER);
+	beep_chord(c4, c6, QUAVER);
+	beep_chord(f3, bb5, QUAVER);
+	beep_chord(c4, ab5,  QUAVER);
+	beep_chord(ab3, ab5, QUAVER);
+	beep_chord(c4, ab5, QUAVER);
+
+	// 28
+	beep_chord(c3, g5, QUAVER);
+	beep_chord(g3, g5,  QUAVER);
+	beep_chord(eb3, g5, QUAVER);
+	beep_chord(g3, f5, QUAVER);
+	beep_chord(c3, eb5, QUAVER);
+	beep_chord(g3, f5,  QUAVER);
+	beep_chord(eb3, f5, QUAVER);
+	beep_chord(g3, g5, QUAVER);
+
+	// 29
+	beep_chord(c3, g5, QUAVER);
+	beep_chord(g3, g5,  QUAVER);
+	beep_chord(eb3, g5, QUAVER);
+	beep_chord(g3, g5, QUAVER);
+	beep_chord(c3, 1, QUAVER);
+	beep_chord(g3, 1,  QUAVER);
+	beep_chord(eb3, c5, QUAVER);
+	beep_chord(g3, c5, QUAVER);
+
+	// 30
+	beep_chord(f3, ab5, QUAVER);
+	beep_chord(c4, ab5,  QUAVER);
+	beep_chord(ab3, ab5, QUAVER);
+	beep_chord(c4, ab5, QUAVER);
+	beep_chord(f3, ab5, QUAVER);
+	beep_chord(c4, ab5,  QUAVER);
+	beep_chord(ab3, bb5, QUAVER);
+	beep_chord(c4, c6, QUAVER);
+	
+	// 31
+	beep_chord(ab3, c6, QUAVER);
+	beep_chord(eb4, c6,  QUAVER);
+	beep_chord(c4, 1, QUAVER);
+	beep_chord(eb4, c6, QUAVER);
+
+	beep_chord(ab3, c6, QUAVER);
+	beep_chord(eb4, c6,  SIXTH);
+	beep_chord(eb4, bb5,  SIXTH*2);
+	beep_chord(c4, bb5, SIXTH*2);
+	beep_chord(c4, ab5, SIXTH);
+	beep_chord(eb4, ab5, QUAVER);
+
+	// 32
+	beep_chord(d5, g5, QUAVER);
+	beep_chord(gb2, 1, QUAVER);
+	beep_chord(g2, 1, QUAVER);
+	beep(r, QUAVER);
+	beep_chord(eb3, 1, QUAVER);
+	beep_chord(d3, 1, QUAVER);
+	beep(r, QUAVER);
+	beep_chord(bb2, 1, QUAVER);
+
+	// 33
+	beep_chord(b2, 1, QUAVER);
+	beep_chord(d3, 1, QUAVER);
+	beep_chord(1, ab3, QUAVER);
+	beep_chord(g2, g3, QUAVER*2);
+	beep_chord(g2, g3, QUAVER);
+	beep_chord(g3, c4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+
+	// 34
+	// TODO: chords
+	beep_chord(c3, g4, QUAVER);
+	beep_chord(g3, g4, QUAVER);
+	beep_chord(eb3, g4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(c3, f4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(eb3, eb4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+	
+	// 35
+	beep_chord(g2, d4, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+	beep_chord(d3, f4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(g2, 1, QUAVER);
+	beep_chord(f3, g3, QUAVER);
+	beep_chord(d3, b3, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+
+	// 36
+	beep_chord(g2, f4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(d3, g4, QUAVER);
+	beep_chord(f3, f4, QUAVER);
+	beep_chord(g2, eb4, QUAVER);
+	beep_chord(f3, d4, QUAVER);
+	beep_chord(d3, d4, QUAVER);
+	beep_chord(f3, c4, QUAVER);
+
+	// 37
+	beep_chord(c3, c4, QUAVER);
+	beep_chord(g3, c4, QUAVER);
+	beep_chord(eb3, eb4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+	beep_chord(c3, 1, QUAVER);
+	beep_chord(g3, 1, QUAVER);
+	beep_chord(eb3, c4, QUAVER);
+	beep_chord(g3, eb4, QUAVER);
+
+	// 38
+	beep_chord(c3, g4, QUAVER);
+	beep_chord(g3, g4, QUAVER);
+	beep_chord(eb3, g4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(c3, f4, QUAVER);
+	beep_chord(g3, f4, QUAVER);
+	beep_chord(eb3, g4, QUAVER);
+	beep_chord(g3, g4, QUAVER);
+
+	// 39
+	beep_chord(f2, bb4, QUAVER);
+	beep_chord(c3, ab4, QUAVER);
+	beep_chord(ab2, ab4, QUAVER);
+	beep_chord(c3, ab4, QUAVER);
+	beep_chord(f2, 1, QUAVER);
+	beep_chord(c3, ab4, QUAVER);
+	beep_chord(ab2, g4, QUAVER);
+	beep_chord(c3, f4, QUAVER);
+
+	// 40
+	beep_chord(g2, g4, QUAVER);
+	beep_chord(g3, g4,  QUAVER);
+	beep_chord(ab2, f4, QUAVER);
+	beep_chord(ab3, f4, QUAVER);
+	beep_chord(a2, eb4, QUAVER);
+	beep_chord(b2, d4, QUAVER);
+	beep_chord(b3, d4, QUAVER);
+	beep_chord(c3, c4, QUAVER);
+
+	// 41
+	beep_chord(c3, c4, QUAVER);
+	beep_chord(c2, c5, QUAVER);
+	beep_chord(c2, c5, QUAVER);
+	beep_chord(c2, c5, QUAVER);
+	beep_chord(c2, c5, QUAVER*2);
+	beep_chord(c2, g5, QUAVER);
+	beep_chord(c2, f5, QUAVER);
+
+	// 42
+	beep_chord(g2, g5, QUAVER);
+	beep_chord(g3, g5, QUAVER);
+	beep_chord(ab2, f5, QUAVER);
+	beep_chord(ab3, f5, QUAVER);
+	beep_chord(a2, eb5, QUAVER);
+	beep_chord(b2, d5, QUAVER);
+	beep_chord(b3, d5, QUAVER);
+	beep_chord(c3, c5, QUAVER);
+
+	// 43
+	beep_chord(c3, c5, QUAVER);
+	beep_chord(c2, c5, QUAVER);
+	beep_chord(c2, 1, QUAVER);
+	beep_chord(c2, 1, QUAVER);
+	beep_chord(c2, 1, QUAVER);
+	beep_chord(c2, 1, QUAVER);
+	beep_chord(c2, ab5, QUAVER);
+	beep_chord(c2, g5, QUAVER);
+	beep_chord(c2, f5, QUAVER);
+
+	// 44
+	beep_chord(g2, g4, QUAVER);
+	beep_chord(g3, g4,  QUAVER);
+	beep_chord(ab2, ab4, QUAVER);
+	beep_chord(ab3, ab4, QUAVER);
+	beep_chord(a2, bb4, QUAVER);
+	beep_chord(b2, b4, QUAVER);
+	beep_chord(b3, b4, QUAVER);
+	beep_chord(c3, c5, QUAVER);
+
+	// 45
+	beep_chord(c3, c5, QUAVER);
+	beep(r, QUAVER);
+	beep_chord(c2, d4, QUAVER*5);
+	beep_chord(g2, d4, DEMISEMIQUAVER);
+	beep_chord(f2, d4, DEMISEMIQUAVER);
+	beep_chord(eb2, d4, DEMISEMIQUAVER);
+	beep_chord(d2, d4, DEMISEMIQUAVER);
+
+	// 46
+	beep_chord(c2, d3, QUAVER);
+
 	close(console_fd);
 	exit(EXIT_SUCCESS);
 }
 
-void beep(int freq, int length) {
+void beep(int freq, uint64_t length) {
 	struct timespec start;
 	struct timespec time;
 	/* Beep */
@@ -116,14 +509,14 @@ void beep(int freq, int length) {
 	clock_gettime(CLOCK_REALTIME, &start);
 	clock_gettime(CLOCK_REALTIME, &time);
 	uint64_t diff = time_diff(&time, &start);
-	while (diff < 1000000*length) {
+	while (diff < length) {
 		clock_gettime(CLOCK_REALTIME, &time);
 		diff = time_diff(&time, &start);
 	}
 	do_beep(0, console_fd);
 }
 
-void beep_chord(int freq1, int freq2, int length) {
+void beep_chord(int freq1, int freq2, uint64_t length) {
 	struct timespec start;
 	struct timespec time;
 	uint64_t diff;
@@ -139,7 +532,7 @@ void beep_chord(int freq1, int freq2, int length) {
 	clock_gettime(CLOCK_REALTIME, &time);
 	diff  = time_diff(&time, &start) * CLOCK_TICK_RATE / 1000000000;
 
-	while (diff < 1000000lu * length * CLOCK_TICK_RATE / 1000000000) {
+	while (diff < length * CLOCK_TICK_RATE / 1000000000) {
 		next_tick = min(period1 * sample1, period2 * sample2);
 		if (next_tick == period1 * sample1) {
 			sample1++;
@@ -147,11 +540,12 @@ void beep_chord(int freq1, int freq2, int length) {
 	       	if (next_tick == period2 * sample2) {
 			sample2++;
 		}
-		if (next_tick < sample + 80) {
-			continue;
-		}
-		ioctl(console_fd, KIOCSOUND, 40);
-		while (diff < sample + 80) {
+		/* 
+		 * The following two magic numbers are just that - they sound
+		 * good on George's machine, who knows why.
+		 */
+		ioctl(console_fd, KIOCSOUND, 40000);
+		while (diff < sample + 200) {
 			clock_gettime(CLOCK_REALTIME, &time);
 			diff = time_diff(&time, &start) * CLOCK_TICK_RATE / 1000000000;
 		}
